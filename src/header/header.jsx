16e83b7b69428ebf249bcca11301c1f9944fe75d
Robertas Baravikov <robka.bar@gmail.com>
2023-12-28 15:27:12 +0200

    asdf

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -2,7 +2,7 @@ import './header.css'
 import lines from "../images/lines.jpg"
 
 const Header = ({title, lineOne, lineTwo, buttonName}) => {
-    return <div style={{ backgroundImage: `url(${lines}.jpg)` }} className="header"><div className="blur"><h1>{title}</h1><p>{lineOne}<br/>{lineTwo}</p><button>{buttonName}</button></div></div>
+    return <div style={{ backgroundImage: `url(${lines})` }} className="header"><div className="blur"><h1>{title}</h1><p>{lineOne}<br/>{lineTwo}</p><button>{buttonName}</button></div></div>
 }
 
 export default Header
